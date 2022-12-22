64951d1e74d5a3fb353906473b5bbf1674ac06d6
gato365 <immanuelwilliams@gato365.com5>
2022-12-22 05:36:02 -0800

    Selected Data

--- statistics/00-gather-clean-real-data.R
+++ statistics/00-gather-clean-real-data.R
@@ -7,5 +7,12 @@ weight_df = readxl::read_xlsx("~/Important_Files/Life/emans_health.xlsx",
 
 workout_df = readxl::read_xlsx("~/Important_Files/Life/emans_health.xlsx",
                                sheet = 2) %>% 
-  filter(is_power == 1,`completion of workout` == 'Yes')
+  filter(is_power == 1,`completion of workout` == 'Yes') %>% 
+  select(Date,muscles_group,exercises,`set 1 (lbs. - #)`,
+         `set 2 (lbs. - #)`,`set 3 (lbs. - #)`) %>% 
+  mutate(muscles_group = case_when(
+    muscles_group == "Shoulders" ~ "Back",
+    TRUE ~ muscles_group
+  ) 
+  )
 
