d9a688a299fab040d728817c40ed5e42fcef302e
gato365 <immanuelwilliams@gato365.com5>
2022-12-22 07:07:31 -0800

    Just checking

--- statistics/import-data.py
+++ statistics/import-data.py
@@ -2,12 +2,24 @@
 ## Import Libraries
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
-## Test 1 Connection to Data Base
+## Task 1: Connection to Data Base (Not Completed)
 weight_df = pd.read_csv('weight_df.csv')
 workout_df = pd.read_csv('workout_df.csv')
 
-## View Data Frame 
-print(weight_df)
+## Task 2: Create a power score using workout df(Not Completed)
+
+## Task 3: Create 5 plots to display information (Not Completed)
+
+## ~~  Plot 1
+x = weight_df.Date
+y = weight_df.Weight_AM
+
+print(y)
+# plt.scatter(x, y)
+# plt.show()
+## Task i: (Not Completed)
+## Task i: (Not Completed)
