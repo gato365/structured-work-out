d0b339257b2db30b75b434a8359ebbcfd5bf186c
gato365 <immanuelwilliams@gato365.com>
2022-12-31 01:26:26 -0800

    Added more details to schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,39 @@ CREATE DATABASE structure_db;
 
 USE structure_db;
 
+-- Initial Survey
 
---User Table--
 CREATE TABLE user (
-    id INT AUTO_INCREMENT NOT NULL,
+    userid INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL,
     username VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(userid)
 );
 
---Exercise Table--
 
 
---Workout Table--
+-- Exercise Table
+CREATE TABLE exercise (
+    muscle_group VARCHAR(50) NOT NULL,
+    exercise VARCHAR(20) NOT NULL 
+);
+
+
+-- Workout Table
+CREATE TABLE workout (
+    id INT AUTO_INCREMENT NOT NULL,
+    userid INT NOT NULL,
+    muscle_group VARCHAR(50) NOT NULL,
+    date DATETIME,
+    exercise VARCHAR(20) NOT NULL,
+    weight INT,
+    number_of_sets INT,
+    number_of_reps INT,
+    FOREIGN KEY (muscle_group) REFERENCES exercise(muscle_group),
+    FOREIGN KEY (userid) REFERENCES user(userid)
+);
+
+-- Survey
