bb74a4d4aa9a6dd708c24ae4714694038596f24c
gato365 <immanuelwilliams@gato365.com>
2022-12-31 01:04:38 -0800

    Added Schema for user table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,20 @@ DROP DATABASE IF EXISTS structure_db;
 CREATE DATABASE structure_db;
 
 USE structure_db;
+
+
+--User Table--
+CREATE TABLE user (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+--Exercise Table--
+
+
+--Workout Table--
