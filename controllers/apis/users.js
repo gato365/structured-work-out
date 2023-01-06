688532c7439a44912d5f920718d1b9d9bc89554d
Ahneb <benjaminha1996@gmail.com>
2023-01-05 18:40:26 -0800

    addded font awesome added 401 error fixed username: email key for fetch error

--- controllers/apis/users.js
+++ controllers/apis/users.js
@@ -1,9 +1,18 @@
 const { Router } = require('express');
 
+const User = require('../../models/User');
+
 const userRouter = new Router();
 
 userRouter.post('/login', async(req, res) => {
-    console.log(req.body);
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ where: {username}});
+
+    if (!user) {
+        res.status(401).end('User not Found');
+        return;
+    };
 
     res.end();
 })
